4ad7668603a370d6582e6db9abc24c153ce21fe0
macabre0408 <macabreOlade@gmail.com>
2024-03-02 22:17:54 +0000

    la classe Categorie

---
+++ src/entities/Categorie.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entities;
+
+/**
+ *
+ * @author dell
+ */
+public class Categorie {
+    private int id;
+    private String designation;
+
+    public Categorie() {
+    }
+
+    public Categorie(int id, String designation) {
+        this.id = id;
+        this.designation = designation;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+    
+}
